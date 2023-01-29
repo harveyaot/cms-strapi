70a9aefa6634b9dd0e59a858ddc4d7b75d30ece6
Wei He <harveyaot@gmail.com>
2023-01-29 17:09:02 +0800

    update datebase config

--- config/database.js
+++ config/database.js
@@ -1,20 +1,26 @@
 const path = require('path');
 
 module.exports = ({ env }) => ({
+/*
   defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL'),
-      },
-      options: {}
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+*/
+// start
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL', false),
+      }
     },
+    debug: false,
   },
 });
