1789102026aac2c506419cbd627d8d84bfcc4449
Wei He <harveyaot@gmail.com>
2023-01-29 15:20:34 +0800

    setup env

--- config/database.js
+++ config/database.js
@@ -1,11 +1,20 @@
 const path = require('path');
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL'),
+      },
+      options: {}
     },
-    useNullAsDefault: true,
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,7 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: "https://cms-strapi-longriver.azurewebsites.net",
   app: {
     keys: env.array('APP_KEYS'),
   },
+  admin:{
+    url: "https://ashy-desert-0929ade1e.2.azurestaticapps.net"
+  }
 });
